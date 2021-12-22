438b455b62195f0c58253ce35c4c658fa3b30f0a
Shreevari SP <shreevari.sp@nutanix.com>
2021-12-22 13:11:32 +0530

    generate docs

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/plugin"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/terraform-providers/terraform-provider-nutanix/nutanix"
 )
+
+// Generate docs for website
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
